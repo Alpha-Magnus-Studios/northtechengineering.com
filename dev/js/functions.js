5c5a84880273c83aa28aae44d3a95f0a2222b973
Afaan Bilal <afaanbilal@gmail.com>
2017-12-23 20:54:28 +0530

    ClientRequest #3 - Submenu for products and services

--- dev/js/functions.js
+++ dev/js/functions.js
@@ -28,7 +28,26 @@ function navbar(page = "index")
             <ul class="nav navbar-nav">
                 <li` + ((page == "index")    ? ' class="active"' : '') + `><a href="index.html">Home</a></li>
                 <li` + ((page == "about")    ? ' class="active"' : '') + `><a href="about.html">About</a></li>
-                <li` + ((page == "pns")      ? ' class="active"' : '') + `><a href="products-and-services.html">Products &amp; Services</a></li>      
+                <li` + ((page == "pns")      ? ' class="active"' : '') + ` class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Products &amp; Services <b class="caret"></b></a>
+                    <ul class="dropdown-menu agile_short_dropdown">
+                        <li><a href="products-and-services.html">All</a></li>
+                        <li><a href="products-and-services.html#cons">Construction</a></li>
+                        <li><a href="products-and-services.html#adm">Admixtures / Construction Chemicals</a></li>
+                        <li><a href="products-and-services.html#gna">Grouts &amp; Anchors</a></li>
+                        <li><a href="products-and-services.html#wp">Waterproofing</a></li>
+                        <li><a href="products-and-services.html#floor">Industrial / Residential  Flooring</a></li>
+                        <li><a href="products-and-services.html#retro">Structural Repairs &amp; Retrofitting</a></li>
+                        <li><a href="products-and-services.html#prefab">Pre-Fabricated Structures</a></li>
+                        <li><a href="products-and-services.html#peb">Pre Engineered Structures (PEB)</a></li>
+                        <li><a href="products-and-services.html#mt">Material Testing</a></li>
+                        <li><a href="products-and-services.html#ce">Construction Equipments</a></li>
+                        <li><a href="products-and-services.html#ndt">Non Destructive Testing (NDT)</a></li>
+                        <li><a href="products-and-services.html#survey">Survey Equipments and Power Tools</a></li>
+                        <li><a href="products-and-services.html#arch">Architectural / Structural Consulting</a></li>
+                        <li><a href="products-and-services.html#gis">Geographic Information Science (GIS)</a></li>
+                    </ul>
+                </li>
                 <li` + ((page == "contact")  ? ' class="active"' : '') + `><a href="contact.html">Contact Us</a></li>
             </ul>
         </nav>
